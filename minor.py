fb7f78257be87122badc253729790c53fb002ab1
jayanshisinha-ui <sinhajayanshi@gmail.com>
2025-12-24 14:34:13 +0530

    1st Commit

---
+++ minor.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("retail_profit_margin.csv")
+print(df)
+
+# formula for calculating the profit margin 
+df["Profit_Margin"] = ((df["Selling_Price"] - df["Cost_Price"])  / df["Cost_Price"]) * 100
+print(df[["Item_Name", "Profit_Margin"]])
+
+# Product Category comparison 
+category_groups = df.groupby("Category")
+print(category_groups.size())
+
+# Calculating mean 
+mean_profit = category_groups["Profit_Margin"].mean()
+print("Mean Profit Margin by Category:\n", mean_profit)
+
+# Calculating standrad deviation
+std_profit = category_groups["Profit_Margin"].std()
+print("Standard Deviation by Category:\n", std_profit)
+
+#Combined Mean & Standard Deviation Table
+stats_table = category_groups["Profit_Margin"].agg(["mean", "std"])
+print(stats_table)
+
+#Box Plot for Profit Margin
+plt.figure(figsize=(8,5))
+sns.boxplot(x='Category', y='Profit_Margin', data=df)
+plt.title('Profit Margin Distribution by Category')
+plt.xlabel('Product Category')
+plt.ylabel('Profit Margin (%)')
+plt.show()
