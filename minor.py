cf6137722ac2e287fd11e8dcab3622f6b5be01ef
jayanshisinha-ui <sinhajayanshi@gmail.com>
2025-12-26 11:25:53 +0530

    minor.py

--- minor.py
+++ minor.py
@@ -7,30 +7,30 @@ df = pd.read_csv("retail_profit_margin.csv")
 print(df)
 
 # Calculating the profit margin 
-df["Profit_Margin"] = ((df["Selling_Price"] - df["Cost_Price"])  / df["Cost_Price"]) * 100
-print(df[["Item_Name", "Profit_Margin"]])
+df["Profit Margin"] = ((df["Selling_Price"] - df["Cost_Price"])  / df["Cost_Price"]) * 100
+print(df[["Item_Name", "Profit Margin"]])
 
 # Product Category comparison 
-category_groups = df.groupby("Category")
-print(category_groups.size())
+category_grp = df.groupby("Category")
+print(category_grp.size())
 
 # Calculating mean 
-mean_profit = category_groups["Profit_Margin"].mean()
-print("Mean Profit Margin by Category:\n", mean_profit)
+mean = category_grp["Profit Margin"].mean()
+print("Mean Profit Margin by Category:\n", mean)
 
 # Calculating standrad deviation
-std_profit = category_groups["Profit_Margin"].std()
-print("Standard Deviation by Category:\n", std_profit)
+std = category_grp["Profit Margin"].std()
+print("Standard Deviation by Category:\n", std)
 
 #Combined Mean & Standard Deviation Table
-stats_table = pd.DataFrame()
-stats_table['Mean']=mean_profit
-stats_table['Standard Deviation']=std_profit
-print(stats_table)
+s_table = pd.DataFrame()
+s_table['Mean']=mean
+s_table['Standard Deviation']=std
+print(s_table)
 
 #Box Plot for Profit Margin
 plt.figure(figsize=(8,5))
-sns.boxplot(x='Category', y='Profit_Margin', data=df)
+sns.boxplot(x='Category', y='Profit Margin', data=df)
 plt.title('Retail Store Item Profit Margin Analysis by Category')
 plt.xlabel('Product Category')
 plt.ylabel('Profit Margin (%)')
